4a499a906a8bba33f5c3c86b63e1ec9b54bbac82
houssam <houssambenlagha0@gmail.com>
2023-04-18 18:00:07 +0100

    title done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel with </title>
+        <title>I'm just trying to learn github</title>
 
         @vite('resources/css/app.css')
     </head>
